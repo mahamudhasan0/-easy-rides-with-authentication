66e0e859e062fc44ee110940bafacfd97662ac8b
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-05-31 23:43:15 +0600

    update firebase authentication

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBzj632XGvKSdD7V4s32MPXLgH9w_VWBrI",
-  authDomain: "ride-9033c.firebaseapp.com",
-  projectId: "ride-9033c",
-  storageBucket: "ride-9033c.appspot.com",
-  messagingSenderId: "116143346561",
-  appId: "1:116143346561:web:29efdb5c37fbca43e8ce80"
+  apiKey: "AIzaSyBYbPzk5sfJR8l71ilmCzN-8Tnv_NynMOo",
+  authDomain: "riders-76f5f.firebaseapp.com",
+  projectId: "riders-76f5f",
+  storageBucket: "riders-76f5f.appspot.com",
+  messagingSenderId: "904093103393",
+  appId: "1:904093103393:web:7a0850ffa37f314068ba60"
 };
